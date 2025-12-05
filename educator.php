b27201e562edea158135287f52024141da6434b8
Achraf-podcast <achrafagourram6@gmail.com>
2025-12-05 10:22:41 +0100

    Added the feature whe nuser click edit button it shows a responsive formulaire for edit animal

--- educator.php
+++ educator.php
@@ -14,8 +14,9 @@ if(isset($_POST['addAnimal'])){
 }
 if(isset($_POST['delete'])){
     $id = $_POST['delete'];
-    mysqli_query($database, "DELETE FROM Animal WHERE id = $id;");
+    //mysqli_query($database, "DELETE FROM Animal WHERE id = $id;");
 }
+
 get_animals("SELECT id, name_animal, image_animal, type_alimentaire, Habitat.name_hab FROM Animal JOIN Habitat ON Animal.habitat_id = Habitat.id_hab;");
 ?>
 
@@ -39,6 +40,52 @@ get_animals("SELECT id, name_animal, image_animal, type_alimentaire, Habitat.nam
             <form method="post"><button type="submit" name="back" class="bg-red-500 text-white font-bold px-6 py-3 rounded-lg hover:bg-red-600 transition duration-300 mb-6 flex items-center"><?= $dict[$currentLang][30]?></button></form>
         </div>
         
+        <?php
+            if(isset($_POST['edit'])){
+                echo "
+                    <div class='bg-green-50 p-6 rounded-lg shadow-inner mb-8' id='form-edit'>
+                        <h3 class='text-2xl font-semibold text-green-700 mb-4'>{$dict[$currentLang][31]}</h3>
+                        <form method='POST' class='grid grid-cols-1 md:grid-cols-2 gap-6' enctype='multipart/form-data'>
+                            <div>
+                                <label for='nom' class='block text-sm font-medium text-gray-700'>{$dict[$currentLang][21]} :</label>
+                                <input type='text' name='name' required class='mt-1 w-full p-2 border border-gray-300 rounded-md'>
+                            </div>
+
+                            <div>
+                                <label for='habitat' class='block text-sm font-medium text-gray-700'>Habitat :</label>
+                                <select id='habitat' name='habitat_id' required class='mt-1 w-full p-2 border border-gray-300 rounded-md'>
+                                    <option value='1'>Savane</option>
+                                    <option value='2'>Forêt</option>
+                                    <option value='3'>Aquatique</option>
+                                    <option value='4'>Desert</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <label for='regime' class='block text-sm font-medium text-gray-700'>{$dict[$currentLang][16]} :</label>
+                                <select id='regime' name='type_alimentaire' required
+                                    class='mt-1 w-full p-2 border border-gray-300 rounded-md'>
+                                    <option value='carnivore'>Carnivore</option>
+                                    <option value='herbivore'>Herbivore</option>
+                                    <option value='omnivore'>Omnivore</option>
+                                </select>
+                            </div>
+
+                            <div>
+                                <label for='image' class='block text-sm font-medium text-gray-700'>{$dict[$currentLang][22]} :</label>
+                                <input type='file' required name='image' accept='image/*' class='mt-1 w-full p-2 border border-gray-300 rounded-md'>
+                            </div>
+
+                            <div class='md:col-span-2 flex justify-end space-x-4'>
+                                <button type='submit' name='editAnimal'
+                                    class='bg-green-500 text-white font-bold px-6 py-2 rounded-lg hover:bg-green-600 transition'>{$dict[$currentLang][23]}</button>
+                                <button type='button' name='cancelEdit' class='bg-gray-300 text-gray-800 font-bold px-6 py-2 rounded-lg hover:bg-gray-400 transition'>{$dict[$currentLang][24]}</button>
+                            </div>
+                        </form>
+                    </div>
+                ";
+            }
+        ?>
 
         <div id="form-ajout" class="bg-green-50 p-6 rounded-lg shadow-inner mb-8 hidden">
             <h3 class="text-2xl font-semibold text-green-700 mb-4"><?= $dict[$currentLang][20]?></h3>
@@ -122,6 +169,9 @@ get_animals("SELECT id, name_animal, image_animal, type_alimentaire, Habitat.nam
             </table>
         </div>
     </div>
+    <script>
+        document.querySelector('[name="cancelEdit"]').addEventListener("click", () => {document.getElementById('form-edit').classList.add('hidden')});
+    </script>
 </body>
 
 </html>
